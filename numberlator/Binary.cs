e1588bd4f33213768e38c91c9262c48cb7c0d47e
94rk <justdarkmoth@gmail.com>
2025-12-04 21:20:57 +0100

    Added dec2bin

---
+++ numberlator/Binary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberlator
+{
+    internal class Binary
+    {
+        public static string DecToBin(int input)
+        {
+            string result = "";
+            List<int> cache = new();
+
+            while (input != 0)
+            {
+                bool even = input % 2 == 0;
+                cache.Add(even ? 0 : 1);
+
+                input /= 2;
+            }
+
+            cache.Reverse();
+
+            foreach (int num in cache)
+                result += num.ToString();
+
+            int targetLength = (int)Math.Pow(2, Math.Ceiling(Math.Log2(Math.Max(result.Length, 4))));
+            int missingLength = targetLength - result.Length;
+
+            for (int i = 0; i < missingLength; i++)
+                result = "0" + result;
+
+            return result;
+        }
+
+        public static string BinToDec(string input)
+        {
+            string result = "";
+            int nearestPow2 = (int)Math.Pow(2, Math.Ceiling(Math.Log2(Math.Max(input, 4))));
+
+            return nearestPow2.ToString();
+        }
+    }
+}
---
+++ numberlator/main.cs
@@ -0,0 +1,69 @@
+﻿namespace numberlator
+{
+    internal class main
+    {
+        // types: decimal, binary
+
+        static void Main(string[] args)
+        {
+            bool running = true;
+
+            while (running)
+            {
+                string inType = "none";
+                string outType = "none";
+
+                int input = 0;
+                string output = "";
+
+                Console.WriteLine("Valid types: decimal, binary \n");
+
+                Console.Write("Enter the input type: ");
+                inType = Console.ReadLine() ?? "none";
+
+                Console.Write("Enter the output type: ");
+                outType = Console.ReadLine() ?? "none";
+
+                Console.WriteLine();
+
+                Console.Write("Enter the input number: ");
+                float.TryParse(Console.ReadLine(), out float f);
+                input = (int)Math.Floor(f);
+
+                Console.WriteLine();
+
+                switch (inType)
+                {
+                    default: break;
+
+                    case "decimal":
+                        if (outType == "binary")
+                        {
+                            output = Binary.DecToBin(input);
+                        }
+                        else if (outType == "decimal")
+                        {
+                            output = input.ToString();
+                        }
+
+                        break;
+
+                    case "binary":
+                        Console.WriteLine("Not implemented");
+
+                        break;
+                        if (outType == "binary")
+                        {
+                            output = input.ToString();
+                        }
+                        else if (outType == "decimal")
+                        {
+                            output = Binary.BinToDec(input.ToString());
+                        }
+                }
+
+                Console.WriteLine(output + "\n");
+            }
+        }
+    }
+}
